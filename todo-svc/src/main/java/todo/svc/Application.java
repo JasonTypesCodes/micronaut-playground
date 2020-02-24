7328330f44b339d7ecac0fc16b8079c00423dbdb
Jason Schindler <schindlerj@objectcomputing.com>
2020-02-24 14:54:01 -0600

    Adds todo service as generated from cli

---
+++ todo-svc/src/main/java/todo/svc/Application.java
@@ -0,0 +1,10 @@
+package todo.svc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
